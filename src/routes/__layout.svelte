6a0a1cf2d1579f079503e1654ad9ff13aa431185
Aditya Raj <aditya.rajb@gmail.com>
2022-01-28 17:18:43 +0530

    make basic page while i design the actual site

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	import '../styles/normalize.css';
+	import '../styles/global.css';
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<div class="hero">
+		<p class="hero__intro">Hi, I'm Aditya</p>
+		<h1 class="hero__title">I make websites</h1>
+	</div>
+</div>
+
+<style>
+	.hero {
+		height: 80vh;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: center;
+	}
+
+	.hero__intro {
+		font-size: 1.25rem;
+		font-weight: 400;
+		margin: 0;
+		margin-bottom: 1.5rem;
+		padding-left: 2px;
+		color: var(--fg);
+	}
+
+	.hero__title {
+		font-size: 6rem;
+		font-weight: 500;
+		margin: 0;
+		color: var(--fg);
+		line-height: 1;
+	}
+
+	@media screen and (max-width: 640px) {
+		.hero__title {
+			font-size: 4rem;
+		}
+	}
+</style>
